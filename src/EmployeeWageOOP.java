e73b4bfa5ef3cadb59ee25cd60e2cb736f35d0cc
subzero <shubham8111994@gmail.com>
2023-02-08 23:32:13 +0530

    added feature for attendance check

--- src/EmployeeWageOOP.java
+++ src/EmployeeWageOOP.java
@@ -1,5 +1,13 @@
 public class EmployeeWageOOP {
+
+    public int empCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+        return empCheck;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        EmployeeWageOOP empCheck = new EmployeeWageOOP();
+        int isPresent = empCheck.empCheck();
+
     }
 }
