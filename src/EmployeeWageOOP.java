0252bbd1fd8d6d9c43af7d3c2d13030adb88f729
subzero <shubham8111994@gmail.com>
2023-02-09 00:05:50 +0530

    added feature to calculate daily wage of full timer

--- src/EmployeeWageOOP.java
+++ src/EmployeeWageOOP.java
@@ -1,13 +1,27 @@
 public class EmployeeWageOOP {
-
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public int empCheck(){
         int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
+    public int dailyWageOfEmployee(int empCheck){
+        int empHrs = 0;
+        int totalWage = 0;
+        switch (empCheck) {
+            case IS_FULL_TIME: //checking if full time or not
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        return totalWage;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        EmployeeWageOOP empCheck = new EmployeeWageOOP();
-        int isPresent = empCheck.empCheck();
-
+        EmployeeWageOOP employee = new EmployeeWageOOP();
+        int isPresent = employee.empCheck();
+        System.out.println(employee.dailyWageOfEmployee(isPresent));
     }
 }
